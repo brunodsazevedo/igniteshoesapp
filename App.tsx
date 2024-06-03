4e0e0177ba0414422d47b25e3a4fe8080bb63081
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-06-03 07:45:00 -0300

    feat: feito update de pacotes para expo 51 e feito instalação de onesignal, e confguração de dev client em projeto

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal'
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("8be8cf23-84ab-432f-a3ac-7dd5aba44707")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
